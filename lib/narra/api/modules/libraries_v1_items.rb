581d8e3c46fbc896fae246e44d290b2ab0edab15
Michal Mocnak <michal@marigan.net>
2015-10-26 16:35:23 +0000

    Typo fixed

--- lib/narra/api/modules/libraries_v1_items.rb
+++ lib/narra/api/modules/libraries_v1_items.rb
@@ -52,7 +52,8 @@ module Narra
               error_not_authorized! unless public || (roles & [:admin, :author, :contributor, :parent_author, :parent_contributor]).size > 0
               # get meta values
               metas = library.items.collect { |item| item.get_meta(name: params[:name])}
-              values = metas.select { |meta| !meta.nil? }.collect { |meta| meta.respond_to('each') ? meta.first.value : meta.value }
+              values = metas.select { |meta| !meta.nil? }.collect { |meta| meta.respond_to?('each') ? meta.first.value : meta.value }
+              values = values.join(',').collect { |value| value.strip }.uniq
               # present
               present_ok_generic(:values, values)
             end
