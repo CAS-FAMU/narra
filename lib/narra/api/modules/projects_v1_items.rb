0d00d79fd32cf1df30320ed559da20a2f2bc1964
Michal Mocnak <michal@marigan.net>
2015-10-30 16:17:54 +0100

    Project api revised

--- lib/narra/api/modules/projects_v1_items.rb
+++ lib/narra/api/modules/projects_v1_items.rb
@@ -54,6 +54,22 @@ module Narra
               present_ok(project.items.find(params[:id]), Item, Narra::API::Entities::Item, 'public')
             end
           end
+
+          desc 'Return project item metadata.'
+          post ':name/items/:id/metadata' do
+            return_one_custom(Project, :name, false, [:author]) do |project, roles, public|
+              # get authorized
+              error_not_authorized! unless (roles & [:admin, :author, :contributor]).size > 0 || public
+              # check for selection
+              if params[:fields].nil?
+                # present
+                present_ok_generic_options('metadata', project.items.find(params[:id]).meta, {with: Narra::API::Entities::MetaItem, type: 'item'})
+              else
+                # present selected
+                present_ok_generic_options('metadata', project.items.find(params[:id]).meta.any_in(name: params[:fields]), {with: Narra::API::Entities::MetaItem, type: 'item'})
+              end
+            end
+          end
         end
       end
     end
