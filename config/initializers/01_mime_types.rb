fde514a2c7af8c3002d182076061e8e7b3145262
Michal Mocnak <michal@marigan.net>
2017-07-09 12:16:25 +0200

    Gems updated

--- config/initializers/01_mime_types.rb
+++ config/initializers/01_mime_types.rb
@@ -1,3 +1,24 @@
+#
+# Copyright (C) 2017 CAS / FAMU
+#
+# This file is part of Narra.
+#
+# Narra is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Narra is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Narra. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors: Michal Mocnak <michal@marigan.net>
+#
+
 # Be sure to restart your server when you modify this file.
 
 # Add new mime types for use in respond_to blocks:
